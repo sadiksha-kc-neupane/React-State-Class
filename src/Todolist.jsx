e9972dce84a8027119b52bf9267950c5a18cf998
sadiksha-kc-neupane <kcneupanesadiksha@gmail.com>
2026-07-06 23:30:18 +0545

    made a strcture of Todo

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,7 +1,24 @@
+
+import { useState } from "react";
+
 export default function TodoList() {
+  let [todos , setTodos]= useState(["sample Task"])
+  let [newTodo , setNewTodos]
   return (
     <div>
-      <h1>Todo list</h1>
+     <input placeholder="add a task"></input>
+     <br></br>
+     <br></br>
+     <button>Add a task</button>
+     <br></br>
+     <br></br>
+     <hr></hr>
+     <h4>Task Todo</h4>
+     <ul>
+      {todos.map((todo)=>{
+        return<li>{todo}</li>
+      })}
+     </ul>
     </div>
   );
 }
