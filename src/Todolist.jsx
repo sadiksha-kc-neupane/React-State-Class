fdbe23e5d38174285fecd390d17439ea2bb53dcd
sadiksha-kc-neupane <kcneupanesadiksha@gmail.com>
2026-07-07 14:32:58 +0545

    ToDo using state functanality completed

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -3,21 +3,33 @@ import { useState } from "react";
 
 export default function TodoList() {
   let [todos , setTodos]= useState(["sample Task"])
-  let [newTodo , setNewTodos]
-  return (
+  let [newTodo , setNewTodos] = useState("");
+
+
+  let addNewTask = ()=>{
+    // console.log("addNewTask called", newTodo);
+    setTodos([...todos, newTodo ]);
+    setNewTodos(" ");
+   
+  }
+
+  let updateTodoValue =(event)=>{
+    setNewTodos(event.target.value);
+  }
+  return(
     <div>
-     <input placeholder="add a task"></input>
+     <input  placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
      <br></br>
      <br></br>
-     <button>Add a task</button>
+     <button onClick={addNewTask}>Add a task</button>
      <br></br>
      <br></br>
      <hr></hr>
      <h4>Task Todo</h4>
      <ul>
-      {todos.map((todo)=>{
-        return<li>{todo}</li>
-      })}
+      {todos.map((todo)=>(
+        <li>{todo}</li> 
+      ))}
      </ul>
     </div>
   );
